86ff178f3033511d48b234530e75cb5ba5538fcf
Robert Manson <robmanson@gmail.com>
2021-02-21 20:55:32 -0800

    Clean Up Invocation Instructions
    Renames default "hello" function to "scrape_times"

--- handler.py
+++ handler.py
@@ -6,7 +6,7 @@ from scraper import Scraper
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-def hello(event, context):
+def scrape_times(event, context):
     url = 'https://cityofla.ezlinksgolf.com/index.html#/search'
 
     logger.info('## ENVIRONMENT VARIABLES')
@@ -15,13 +15,10 @@ def hello(event, context):
     screenshot_file = "{}-{}".format(''.join(filter(str.isalpha, url)), str(uuid.uuid4()))
     driver = Scraper()
     
-    # logger.info('Generate fixed height screenshot')
-    # driver.scrape(url, '/tmp/{}-fixed.png'.format(screenshot_file), height=1024)
-    
     logger.info('Scrape EZ Links')
     results = driver.scrape(url, '/tmp/{}-full.png'.format(screenshot_file))
     
-    # driver.close()
+    driver.close()
 
     response = {
         "statusCode": 200,
