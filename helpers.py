d52d96c61ca1fab318c15f7793f4c5330cf2625f
Robert Manson <robmanson@gmail.com>
2021-03-02 22:24:51 -0800

    Name Results File After Date

--- helpers.py
+++ helpers.py
@@ -1,5 +1,13 @@
 from datetime import datetime, timedelta
+from pytz import timezone
 
 def nextSaturday(d=datetime.now()):
 	t = timedelta((7 + 5 - d.weekday()) % 7)
 	return (d + t).strftime('%m/%d/%Y')
+
+def time_in_los_angeles():
+	tz = timezone("America/Los_Angeles")
+	return datetime.now(tz)
+
+def results_filename(d=time_in_los_angeles()):
+	return "rancho_park-" + d.astimezone().isoformat() + ".json"
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,6 +1,7 @@
 import unittest
 
 from datetime import datetime
+from pytz import timezone
 
 from helpers import *
 
@@ -13,3 +14,17 @@ class HelperTest(unittest.TestCase):
 
 	  saturday = nextSaturday(friday)
 	  self.assertEqual(saturday, "01/02/2021")
+
+  def test_file_name(self):
+	  tz = timezone("America/Los_Angeles")
+
+	  startdate = "01-01-2021"
+	  friday = datetime.strptime(startdate, '%m-%d-%Y')
+	  friday.replace(tzinfo=tz)
+
+	  result = results_filename(friday)
+	  self.assertEqual(result, "rancho_park-2021-01-01T00:00:00-08:00")
+
+  def test_time_in_los_angeles(self):
+	  time = time_in_los_angeles()
+	  self.assertEqual(time.tzinfo.zone, 'America/Los_Angeles')
