8915c74141b4f072218fd6e0571b0864ddac3ae3
Robert Manson <robmanson@gmail.com>
2021-03-06 21:42:54 -0800

    Fix Size Issue By Excluding Files

--- helpers.py
+++ helpers.py
@@ -1,13 +1,13 @@
 from datetime import datetime, timedelta
-from pytz import timezone
+from dateutil import tz, parser
 
 def nextSaturday(d=datetime.now()):
 	t = timedelta((7 + 5 - d.weekday()) % 7)
 	return (d + t).strftime('%m/%d/%Y')
 
 def time_in_los_angeles():
-	tz = timezone("America/Los_Angeles")
-	return datetime.now(tz)
+	time_zone = tz.gettz("America/Los_Angeles")
+	return datetime.now(time_zone)
 
 def results_filename(d=time_in_los_angeles()):
 	return "rancho_park-" + d.astimezone().isoformat() + ".json"
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,30 +1,42 @@
 import unittest
 
 from datetime import datetime
-from pytz import timezone
+from dateutil import tz, parser
 
 from helpers import *
 
 class HelperTest(unittest.TestCase):
 
   def test_next_saturday(self):
-	  # January 1st was a Friday
-	  startdate = "01-01-2021"
-	  friday = datetime.strptime(startdate, '%m-%d-%Y')
+      # January 1st was a Friday
+      startdate = "01-01-2021"
+      friday = datetime.strptime(startdate, '%m-%d-%Y')
 
-	  saturday = nextSaturday(friday)
-	  self.assertEqual(saturday, "01/02/2021")
+      saturday = nextSaturday(friday)
+      self.assertEqual(saturday, "01/02/2021")
 
   def test_file_name(self):
-	  tz = timezone("America/Los_Angeles")
+      time_zone = tz.gettz("America/Los_Angeles")
 
-	  startdate = "01-01-2021"
-	  friday = datetime.strptime(startdate, '%m-%d-%Y')
-	  friday.replace(tzinfo=tz)
+      startdate = "01-01-2021"
+      friday = datetime.strptime(startdate, '%m-%d-%Y')
+      friday.replace(tzinfo=time_zone)
 
-	  result = results_filename(friday)
-	  self.assertEqual(result, "rancho_park-2021-01-01T00:00:00-08:00")
+      result = results_filename(friday)
+      self.assertEqual(result, "rancho_park-2021-01-01T00:00:00-08:00.json")
 
   def test_time_in_los_angeles(self):
-	  time = time_in_los_angeles()
-	  self.assertEqual(time.tzinfo.zone, 'America/Los_Angeles')
+      time = time_in_los_angeles()
+      print(dir(time.tzinfo))
+      name = time.tzinfo.tzname(time)
+
+      if "PD" in name:
+          self.assertEqual(
+             name, 
+             'PDT'
+           )
+      else:
+           self.assertEqual(
+              name, 
+              'PST'
+            )
